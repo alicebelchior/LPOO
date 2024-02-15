9f8cd331ce67a01faca604dd1efd25464eef8c67
alicebelchior <alice.belchior23@gmail.com>
2024-02-15 08:42:40 -0300

    Atividades de revisão de lógica 3 e 4

--- revisao/src/revisao/revisao3.java
+++ revisao/src/revisao/revisao3.java
@@ -24,44 +24,47 @@ public class revisao3 {
         
         //Entrada de dados       
         while(!nome.equalsIgnoreCase("fim")){
+        //nome ou fim
             System.out.println("Digite um nome ou 'fim': ");
             nome = sc.next();
             
-            //contagem de mulheres e homens, e o total de pessoas
-            System.out.println("Digite o sexo da pessoa (f/m): ");
-            sexo = sc.next();
-            
-            if (sexo.equalsIgnoreCase("f")){
-                mulher++;
-            } else if(sexo.equalsIgnoreCase("m")) {
-                homem++;
+            // se não for digitado fim
+            if(!nome.equalsIgnoreCase("fim")){
+                //contagem de mulheres e homens, e o total de pessoas
+                System.out.println("Digite o sexo da pessoa (f/m): ");
+                sexo = sc.next();
+
+                if (sexo.equalsIgnoreCase("f")){
+                    mulher++;
+                } else if(sexo.equalsIgnoreCase("m")) {
+                    homem++;
+                }
+
+                totalPessoas++;
+
+                //Mais velho, mais novo e a soma das idades
+                System.out.println("Digite a idade da pessoa: ");
+                idade = sc.nextInt();
+
+                somaIdades += idade;
+
+                if (idade > maisVelho){
+                    maisVelho = idade;
+                    pessoaVelha = nome;
+                } 
+                if(idade < maisNovo) {
+                    maisNovo = idade;
+                    pessoaNova = nome;
+                }
             }
-            
-            totalPessoas++;
-            
-            //Mais velho, mais novo e a soma das idades
-            System.out.println("Digite a idade da pessoa: ");
-            idade = sc.nextInt();
-            
-            somaIdades += idade;
-     
-            if (idade > maisVelho){
-                maisVelho = idade;
-                pessoaVelha = nome;
-            } else {
-                maisNovo = idade;
-                pessoaNova = nome;
-            }
-            
         }
         //média das idades
         mediaIdades = somaIdades / totalPessoas;
         
         //Saída de dados
-        
         System.out.printf("A pessoa mais velha é %s, com %d anos, e a mais nova é %s com %d anos.%n", pessoaVelha, maisVelho, pessoaNova, maisNovo);
         System.out.printf("Existem %d mulheres e %d homens.%n", mulher, homem);
-        System.out.printf("Foram registradas %d pessoas", totalPessoas);
+        System.out.printf("Foram registradas %d pessoas.%n", totalPessoas);
         System.out.printf("A soma das idades é %d. A média, %.2f%n", somaIdades, mediaIdades);
     }
     
--- revisao/src/revisao/revisao4.java
+++ revisao/src/revisao/revisao4.java
@@ -1,5 +1,7 @@
 package revisao;
 
+import java.util.Scanner;
+
 public class revisao4 {
 
     /*
@@ -13,7 +15,38 @@ public class revisao4 {
     */
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+        //Variáveis
+        int numero = 0, numBoiPesado = 0, numBoiLeve = 0, totalBoi = 0;
+        float pesoBoi = 0, pesoTotal = 0, mediaPeso = 0, boiPesado = 0, boiLeve = 10000000;
+        
+        //Entrada de dados
+        while(pesoBoi != 0){ //leitura encerra quando o número digitado for 0
+            System.out.println("Digite o número e o peso do boi (em arrobas):");
+            numero = sc.nextInt();
+            pesoBoi = sc.nextFloat();
+        
+            if(pesoBoi != 0){ //se não for digitado 0
+                //para cada boi registrado, o peso total aumenta
+                pesoTotal += pesoBoi;
+                
+                //para cada boi registrado, a qntd aumenta
+                totalBoi += 1;
+                
+                //qual é o mais pesado
+                if(pesoBoi > boiPesado){
+                    boiPesado = pesoBoi;
+                    numBoiPesado = numero;
+                }
+                //qual é o mais leve
+                if(pesoBoi < boiLeve){
+                    boiLeve = pesoBoi;
+                    numBoiLeve = numero;
+                }
+            }
+        }
+        mediaPeso = pesoTotal / totalBoi;
     }
     
 }
