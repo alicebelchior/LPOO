39e22209b17f4ec32be887aa8a68e641e29a4156
alicebelchior <alice.belchior23@gmail.com>
2024-03-12 07:23:40 -0300

    Término do exercício de polimorfismo

--- Exemplos/src/Polimorfismo/ProgP.java
+++ Exemplos/src/Polimorfismo/ProgP.java
@@ -7,23 +7,23 @@ public class ProgP {
         //cada posição com varios tipos de pessoas
         //milico
         m[0] = new Milico(23, "Juca", "Cabo", "Rio de Janeiro");
-        m[2] = new Milico(50, "Rezende", "Major", "Vitória");
-        m[3] = new Milico(65, "Oliveira", "General", "São Paulo");
+        m[1] = new Milico(50, "Rezende", "Major", "Vitória");
+        m[2] = new Milico(65, "Oliveira", "General", "São Paulo");
         
         //politico
-        m[4] = new Politico(31, "Joao", "PMDB", (float) 98765.65);
-        m[5] = new Politico(56, "Fernandinho", "PTB", (float) 123456.65);
-        m[6] = new Politico(35, "Lule", "Trabalhadores", (float) 142536.56);
+        m[3] = new Politico(31, "Joao", "PMDB", (float) 98765.65);
+        m[4] = new Politico(56, "Fernandinho", "PTB", (float) 123456.65);
+        m[5] = new Politico(35, "Lule", "Trabalhadores", (float) 142536.56);
         
         //professor
-        m[7] = new Professor(20, "Maria", "Português");
-        m[8] = new Professor(5, "Alberto", "Química");
-        m[9] = new Professor(10, "Thamirys", "Física");
+        m[6] = new Professor(20, "Luisa", "Português");
+        m[7] = new Professor(5, "Alberto", "Química");
+        m[8] = new Professor(10, "Thamirys", "Física");
         
         //estagiario
-        m[10] = new Estagiario(28,"Alice", "Informática", 80);
-        m[11] = new Estagiario(18,"Pedro", "Administração", 74);
-        m[12] = new Estagiario(40,"Jonas", "Logística", 93);
+        m[9] = new Estagiario(28,"Alice", "Informática", 80);
+        m[10] = new Estagiario(18,"Pedro", "Administração", 74);
+        m[11] = new Estagiario(40,"Jonas", "Logística", 93);
         
         
         //saida de dado de cada sub-classe
@@ -39,6 +39,5 @@ public class ProgP {
         m[9].mostrar();
         m[10].mostrar();
         m[11].mostrar();
-        m[12].mostrar();
     }
 }
