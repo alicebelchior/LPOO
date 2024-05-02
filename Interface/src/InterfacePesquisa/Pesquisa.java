dd4873629a1cd2984ae594a8234ecead19b12925
alicebelchior <alice.belchior23@gmail.com>
2024-05-02 09:26:21 -0300

    Interface grafica de formulário de pesquisa

--- Interface/src/InterfacePesquisa/Pesquisa.java
+++ Interface/src/InterfacePesquisa/Pesquisa.java
@@ -1,10 +1,8 @@
 package InterfacePesquisa;
 
-import java.text.ParseException;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.text.MaskFormatter;
 import javax.swing.JFormattedTextField;
 
@@ -13,53 +11,53 @@ public class Pesquisa {
     public static void main(String[] args) {
         //config da tela
         JFrame tela = new JFrame("Formulário de Pesquisa");
-        JLabel fundoTela = new JLabel(new ImageIcon("src/trabalhadores.jpg"));
-        tela.setBounds(100, 100, 500, 500);
-        tela.setContentPane(fundoTela);
+        tela.setBounds(100, 100, 600, 500);
         tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
         //config do fundo de tela
-//        ImageIcon fundoTela = new ImageIcon("trabalhadores.jpg");
-//        JLabel fundo = new JLabel(fundoTela);
-//        fundo.setIcon(fundoTela);
-//        fundo.setBounds(100, 100, 500, 500);
-//        tela.revalidate();
-
+        JLabel fundoTela = new JLabel(new ImageIcon("trabalhoadores.jpg"));
+        tela.setContentPane(fundoTela);
 
         //Labels do formulario da pesquisa
         //CPF
         JLabel cpf = new JLabel("CPF");
-        cpf.setBounds(20, 20, 50, 30);
+        cpf.setFont(new Font("Arial", Font.BOLD, 20));
+        cpf.setBounds(35, 30, 100, 30);
         tela.add(cpf);
 
         //CEP
         JLabel cep = new JLabel("CEP");
-        cep.setBounds(20, 60, 50, 30);
+        cep.setFont(new Font("Arial", Font.BOLD, 20));
+        cep.setBounds(35, 70, 100, 30);
         tela.add(cep);
 
         //Telefone
         JLabel fone = new JLabel("Telefone");
-        fone.setBounds(20, 100, 50, 30);
+        fone.setFont(new Font("Arial", Font.BOLD, 20));
+        fone.setBounds(35, 110, 100, 30);
         tela.add(fone);
 
         //CNPJ
         JLabel cnpj = new JLabel("CNPJ");
-        cnpj.setBounds(20, 140, 50, 30);
+        cnpj.setFont(new Font("Arial", Font.BOLD, 20));
+        cnpj.setBounds(35, 150, 100, 30);
         tela.add(cnpj);
 
         //Sigla de estado
         JLabel uf = new JLabel("Estado");
-        uf.setBounds(20, 180, 50, 30);
+        uf.setFont(new Font("Arial", Font.BOLD, 20));
+        uf.setBounds(35, 190, 100, 30);
         tela.add(uf);
 
         //Nome
         JLabel nome = new JLabel("Nome");
-        nome.setBounds(20, 220, 50, 30);
+        nome.setFont(new Font("Arial", Font.BOLD, 20));
+        nome.setBounds(35, 230, 100, 30);
         tela.add(nome);
 
         //e-mail
         JLabel email = new JLabel("E-mail");
-        email.setBounds(20, 260, 50, 30);
+        email.setFont(new Font("Arial", Font.BOLD, 20));
+        email.setBounds(35, 270, 100, 30);
         tela.add(email);
 
         //Campos p/ preenchimento do formulario de pesquisa
@@ -86,32 +84,49 @@ public class Pesquisa {
 
         //configuração das mascaras
         JFormattedTextField cpfFormat = new JFormattedTextField(maskCpf);
-        cpfFormat.setBounds(80, 20, 100, 30);
+        cpfFormat.setBounds(200, 30, 100, 30);
         tela.add(cpfFormat);
 
         JFormattedTextField cepFormat = new JFormattedTextField(maskCep);
-        cepFormat.setBounds(80, 60, 100, 30);
+        cepFormat.setBounds(200, 70, 100, 30);
         tela.add(cepFormat);
 
         JFormattedTextField foneFormat = new JFormattedTextField(maskFone);
-        foneFormat.setBounds(80, 100, 100, 30);
+        foneFormat.setBounds(200, 110, 100, 30);
         tela.add(foneFormat);
 
         JFormattedTextField cnpjFormat = new JFormattedTextField(maskCnpj);
-        cnpjFormat.setBounds(80, 140, 150, 30);
+        cnpjFormat.setBounds(200, 150, 150, 30);
         tela.add(cnpjFormat);
 
         JFormattedTextField ufFormat = new JFormattedTextField(maskUf);
-        ufFormat.setBounds(80, 180, 50, 30);
+        ufFormat.setBounds(200, 190, 50, 30);
         tela.add(ufFormat);
 
         JFormattedTextField nomeFormat = new JFormattedTextField(maskNome);
-        nomeFormat.setBounds(80, 220, 220, 30);
+        nomeFormat.setBounds(200, 230, 220, 30);
         tela.add(nomeFormat);
 
         JFormattedTextField emailFormat = new JFormattedTextField(maskEmail);
-        emailFormat.setBounds(80, 260, 220, 30);
+        emailFormat.setBounds(200, 270, 220, 30);
         tela.add(emailFormat);
+        
+        //config dos icons
+        //icon 1
+        ImageIcon icon1 = new ImageIcon("bp.jpg");
+        JButton btn1 = new JButton();
+        btn1.setIcon(icon1);
+        btn1.setBounds(50, 350, 260, 100);
+        tela.add(btn1);
+        tela.revalidate();
+        
+        //icon 2
+        ImageIcon icon2 = new ImageIcon("rp.jpg");
+        JButton btn2 = new JButton();
+        btn2.setIcon(icon2);
+        btn2.setBounds(311, 350, 260, 100);
+        tela.add(btn2);
+        tela.revalidate();
 
         //saída de dados
         tela.setLayout(null);
